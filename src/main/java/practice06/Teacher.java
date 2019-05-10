44372943e88d9fe3beecc2fe163f2fc78c167be1
guanyingjie <39229309+guanyingjie@users.noreply.github.com>
2019-05-10 20:18:36 +0800

    practice06 finish

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+    public int getKlass(){
+        return klass;
+    }
+    public void setKlass(Integer klass){
+        this.klass=klass;
+    }
+    public Teacher(String name,int age,Integer klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    public Teacher(String name,int age){
+        super(name,age);
+    }
+    public String introduce(){
+        if(this.klass==null)
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        else
+            return  super.introduce()+" I am a Teacher. I teach Class "+this.klass+".";
+    }
+
 }
